0965e2ddeebc69b45dae1883f8ba1a423714a852
mbilalqureshi <bilalqureshi.epic@gmail.com>
2024-05-19 19:34:53 +0200

    Revise Getting & Setting Dictionary Items and add in readme

--- README.md
+++ README.md
@@ -54,3 +54,13 @@ As a list can contain many data types, not all methods will work on all lists
 - To delete a key/value pair use **del** and the key.
 - To list the keys in the dictionary, you can use **list()** or **sorted()** 
 - If you just want to know if a key exists within the dictionary, use the **in** keyword.
+- The order of a dictionary is not fixed. Two people creating the same dictionary on different computers might get a different order of items. 
+- It does not have an index as a list has.
+
+### Getting & Setting Dictionary Items
+- **dict()** function, which can create dictionaries **from lists**.
+- The **fromkeys()** method is used to specify the keys list as the keys and a variable of an empty string as the values.
+- To set a value, you can use the items key in square bracket notation and the assignment operator with a value. The default values are all overridden with the new values.
+- If we attempted to get the value of a **nonexistent key**, then an error would occur. To avoid this you can use the **get()** method which will get the value if the key exists and return **None** if it does not.
+- If we want to get a list of the keys only you can use the **keys()** method and wrap that in a **list()** function. The same syntax but using values or items will get a list of the dictionary values or items instead.
+- See dict.py file to see this in action.
--- dict.py
+++ dict.py
@@ -30,3 +30,42 @@ print(type(sorted(user))) # <class 'list'>
 # Check if key exist in dict
 print('username' in user)
 
+# ---------------------------------- Getting & Setting Dictionary Items ----------------------
+print('Getting & Setting Dictionary Items')
+# Keys are currently only items in list here
+keys = ['username', 'first_name','last_name','age']
+default_value = ''
+
+# Creating dict keys using dict and providing default values using fromkeys
+user_new = dict.fromkeys(keys,default_value)
+print(user_new) # {'username': '', 'first_name': '', 'last_name': '', 'age': ''}
+
+# Updating with new values
+user_new['username'] = 'tombombadil'
+user_new['first_name'] = 'Tom'
+user_new['last_name'] = 'Bombadil'
+user_new['age'] = 100
+print(user_new) # {'username': 'tombombadil', 'first_name': 'Tom', 'last_name': 'Bombadil', 'age': 100}
+
+# Get home , if does not exist just print doesn't exist
+print(user.get('home', "doesn't exist"))
+# Add new home key value pair
+user['home'] = 'Withywindle, Middle-Earth'
+# Now get home , doesn't exist will not be shown now
+print(user.get('home', "doesn't exist"))
+print(user)
+
+# delete home
+del user['home']
+print(user)
+
+# Create list of all the keys
+print(list(user.keys()))
+# Create list of all the values
+print(list(user.values()))
+
+# *** Creating dict from two lists, Advance concept ****
+f = ['a','b']
+g = [1,3]
+new = dict(map(lambda i,j : (i,j) , f,g))
+print(new) # {'a': 1, 'b': 3}
